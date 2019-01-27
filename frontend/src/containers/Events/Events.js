924faa0b5415ed180f1a9988f383d6555f344bdf
Daniel Barclay <daniel@urbanintel.io>
2019-01-27 23:57:40 +0000

    Basic navigation for frontend

---
+++ frontend/src/containers/Events/Events.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Events extends Component {
+    render () {
+        return (
+            <h1>Events</h1>
+        );
+    }
+}
+
+export default Events;
