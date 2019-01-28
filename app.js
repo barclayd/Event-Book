934b59d15788a6c680492fbeb7753700af95412d
Daniel Barclay <barclaysd@me.com>
2019-01-28 13:17:25 +0000

    Frontend connected to GraphQL API to login and sign up users

--- app.js
+++ app.js
@@ -12,19 +12,23 @@ const checkAuth = require('./middleware/check-auth');
 // middleware
 express.json();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(checkAuth);
 
-app.use('/', graphqlHttp({
+app.use('/graphql', graphqlHttp({
     schema: graphQLSchema,
     rootValue: graphQlResolvers,
     graphiql: true
 }));
-
-app.use('/', (req, res, next) => {
-    res.status(200).json({
-        hello: 'Hello World'
-    })
-});
 
 // connect to MongoDB
 mongoose.connect(`mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PW}@cluster0-vxymi.mongodb.net/${process.env.DB_NAME}?retryWrites=true`, {useNewUrlParser: true})
