201bd94bbf99b1af7b14614898eac5f55176faf5
Daniel Barclay <barclaysd@me.com>
2019-01-31 00:46:40 +0000

    Tweaked styling and added functionality to modal to allow new events to be created and stored in MongoDB

--- frontend/src/components/Navigation/navigation.js
+++ frontend/src/components/Navigation/navigation.js
@@ -3,32 +3,35 @@ import {NavLink} from 'react-router-dom';
 import AuthContext from '../../context/auth-context';
 import * as classes from './navigation.module.css';
 
-const navigation = (props) => (
-    <AuthContext.Consumer>
-        {(context) => {
-            return (
-                <header className={classes.header}>
-                    <div className={classes.logo}>
-                        <h1>EventBook</h1>
-                    </div>
-                    <nav className={classes.navigationItems}>
-                        <ul>
-                            <li><NavLink to="/events">Events</NavLink></li>
-                            {context.token && (
-                                <li><NavLink to="/bookings">Bookings</NavLink></li>
-                            )}
-                            {context.token && (
-                                <li><button onClick={context.logout}>Logout</button></li>
-                            )}
-                            {!context.token && (
-                                <li><NavLink to="/login">Login</NavLink></li>
-                            )}
-                        </ul>
-                    </nav>
-                </header>
-            )
-        }}
-    </AuthContext.Consumer>
-);
+const navigation = (props) => {
+
+    return (
+            <AuthContext.Consumer>
+                {(context) => {
+                    return (
+                        <header className={classes.header}>
+                            <div className={classes.logo}>
+                                <h1>EventBook</h1>
+                            </div>
+                            <nav className={classes.navigationItems}>
+                                <ul>
+                                    <li><NavLink to="/events" activeClassName={classes.isActive}>Events</NavLink></li>
+                                    {context.token && (
+                                        <li><NavLink to="/bookings" activeClassName={classes.isActive}>Bookings</NavLink></li>
+                                    )}
+                                    {context.token && (
+                                        <li><button onClick={context.logout}>Logout</button></li>
+                                    )}
+                                    {!context.token && (
+                                        <li><NavLink to="/login" activeClassName={classes.isActive}>Login</NavLink></li>
+                                    )}
+                                </ul>
+                            </nav>
+                        </header>
+                    )
+                }}
+            </AuthContext.Consumer>
+        )
+};
 
 export default navigation;
