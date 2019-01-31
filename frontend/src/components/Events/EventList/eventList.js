f89c858c0d17634e98e948ee92349a8fc58d3210
Daniel Barclay <daniel@urbanintel.io>
2019-01-31 12:05:13 +0000

    Separated Event logic into separate component files. More information dynamically rendered for event, check if logged in user is event creator

---
+++ frontend/src/components/Events/EventList/eventList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import EventItem from './EventItem/eventItem';
+import * as classes from './eventList.module.css';
+
+const eventList = props => {
+
+    const events = props.events.map(event => {
+        console.log(event.creator._id);
+        return <EventItem key={event._id} eventId={event._id} title={event.title} price={event.price} className={classes.listedEvent} creator={event.creator._id === props.owner}/>
+    });
+
+    return (
+    <ul className={classes.eventsList}>
+        {events}
+    </ul>
+    );
+
+};
+
+export default eventList;
