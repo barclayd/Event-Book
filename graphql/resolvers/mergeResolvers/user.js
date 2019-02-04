3bbf3c5ada81dde31385d301a3b70c4ebcb10cd6
Daniel Barclay <daniel@urbanintel.io>
2019-02-04 18:25:44 +0000

    Implemented Dataloader to prevent multiple non-unique database requests to speed up graphQL API

--- graphql/resolvers/mergeResolvers/user.js
+++ graphql/resolvers/mergeResolvers/user.js
@@ -1,4 +1,14 @@
 const User = require('../../../models/user');
+const DataLoader = require('dataloader');
+const Event = require('../../../models/event');
+
+const eventLoader = new DataLoader((eventIds) => {
+    return events(eventIds);
+});
+
+const userLoader = new DataLoader((userIds) => {
+    return User.find({_id: {$in: userIds}});
+});
 
 const events = async eventIds => {
     try {
@@ -19,12 +29,12 @@ const events = async eventIds => {
 
 const user = async userId => {
     try {
-        const user = await User.findById(userId);
+        const user = await userLoader.load(userId.toString());
         return {
             ...user._doc,
             _id: user.id,
             password: null,
-            createdEvents: events.bind(this, user._doc.createdEvents)
+            createdEvents: () => eventLoader.loadMany.bind(this, user._doc.createdEvents)
         };
     } catch (err) {
         throw err;
@@ -33,5 +43,6 @@ const user = async userId => {
 
 module.exports = {
     user,
-    events
+    events,
+    eventLoader
 };
