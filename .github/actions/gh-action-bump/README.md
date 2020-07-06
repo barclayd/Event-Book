6fd632672dbe3cf06f327bed9fc39da677094541
Daniel Barclay <barclaysd@me.com>
2020-07-06 22:51:47 +0100

    add workflow

---
+++ .github/actions/gh-action-bump/README.md
@@ -0,0 +1,39 @@
+## gh-action-bump-version
+
+GitHub Action for automated npm version bump.
+
+This Action bumps the version in package.json and push it back to the repo. 
+It is meant to be used on every successful merge to master but 
+you'll need to configured that workflow yourself. You can look to the
+[`.github/workflows/push.yml`](.github/workflows/push.yml) file in this project as an example.
+
+**Attention**
+
+Make sure you use the `actions/checkout@v2` action!
+
+### Workflow
+
+* Based on the commit messages, increment the version from the latest release.
+  * If the string "BREAKING CHANGE" or "major" is found anywhere in any of the commit messages or descriptions the major 
+    version will be incremented.
+  * If a commit message begins with the string "feat" or includes "minor" then the minor version will be increased. This works
+    for most common commit metadata for feature additions: `"feat: new API"` and `"feature: new API"`.
+  * All other changes will increment the patch version.
+* Push the bumped npm version in package.json back into the repo.
+* Push a tag for the new version back into the repo.
+
+### Usage:
+**tag-prefix:** Prefix that is used for the git tag  (optional). Example:
+```yaml
+- name:  'Automated Version Bump'
+  uses:  'phips28/gh-action-bump-version@master'
+  with:
+    tag-prefix:  ''
+```
+**PACKAGEJSON_DIR:** Param to parse the location of the desired package.json (optional). Example:
+```yaml
+- name:  'Automated Version Bump'
+  uses:  'phips28/gh-action-bump-version@master'
+  env:
+    PACKAGEJSON_DIR:  'frontend'
+```
---
+++ .github/actions/gh-action-bump/index.js
@@ -0,0 +1,102 @@
+const { Toolkit } = require('actions-toolkit');
+const { execSync } = require('child_process');
+
+// Change working directory if user defined PACKAGEJSON_DIR
+if (process.env.PACKAGEJSON_DIR) {
+  process.env.GITHUB_WORKSPACE = `${process.env.GITHUB_WORKSPACE}/${process.env.PACKAGEJSON_DIR}`;
+  process.chdir(process.env.GITHUB_WORKSPACE);
+}
+
+const commitVersion = async (tools, current, version, branch) => {
+  await tools.runInWorkspace('git', ['checkout', branch]);
+  await tools.runInWorkspace('npm', [
+    'version',
+    '--allow-same-version=true',
+    '--git-tag-version=false',
+    current,
+  ]);
+
+  let newVersion = execSync(`npm version --git-tag-version=false ${version}`)
+    .toString()
+    .trim();
+  newVersion = newVersion.substr(1);
+  newVersion = `${process.env['INPUT_TAG-PREFIX']}${newVersion}`;
+
+  const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
+  await tools.runInWorkspace('git', ['tag', newVersion]);
+  await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags']);
+  await tools.runInWorkspace('git', ['push', remoteRepo, '--tags']);
+};
+
+// Run your GitHub Action!
+Toolkit.run(async (tools) => {
+  const pkg = tools.getPackageJSON();
+  const event = tools.context.payload;
+
+  const messages = event.commits.map(
+    (commit) => commit.message + '\n' + commit.body,
+  );
+
+  const commitMessage = 'version bump to';
+  const isVersionBump = messages
+    .map((message) => message.toLowerCase().includes(commitMessage))
+    .includes(true);
+  if (isVersionBump) {
+    tools.exit.success('No action necessary!');
+    return;
+  }
+
+  let version = 'patch';
+  if (
+    messages
+      .map(
+        (message) =>
+          message.includes('BREAKING CHANGE') || message.includes('major'),
+      )
+      .includes(true)
+  ) {
+    version = 'major';
+  } else if (
+    messages
+      .map(
+        (message) =>
+          message.toLowerCase().startsWith('feat') ||
+          message.toLowerCase().includes('minor'),
+      )
+      .includes(true)
+  ) {
+    version = 'minor';
+  }
+
+  try {
+    const current = pkg.version.toString();
+    // set git user
+    await tools.runInWorkspace('git', [
+      'config',
+      'user.name',
+      `"${process.env.GITHUB_USER || 'Automated Version Bump'}"`,
+    ]);
+    await tools.runInWorkspace('git', [
+      'config',
+      'user.email',
+      `"${
+        process.env.GITHUB_EMAIL ||
+        'gh-action-bump-version@users.noreply.github.com'
+      }"`,
+    ]);
+
+    const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(
+      process.env.GITHUB_REF,
+    )[1];
+    console.log('currentBranch:', currentBranch);
+
+    // do it in the current checked out github branch (DETACHED HEAD)
+    // important for further usage of the package.json version
+    await commitVersion(tools, current, version, currentBranch);
+    await commitVersion(tools, current, version, 'develop');
+  } catch (e) {
+    tools.log.fatal(e);
+    tools.exit.failure('Failed to bump version');
+  }
+  tools.exit.success(`Version bumped`);
+});
